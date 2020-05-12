d6d747829c428ec62c3f41d8ab15dc3c40cdfccb
Nishat Farooqui <nishat2604@hotmail.com>
2020-05-12 14:20:06 +0530

    Add project files.

---
+++ BlogApp/Models/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        [DataType(DataType.Date),
+  DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Creation Date")]
+
+        public DateTime? DateCreated { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
---
+++ BlogApp/Models/BloggingContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlogApp.Models
+{
+    public partial class BloggingContext : DbContext
+    {
+        public BloggingContext()
+        {
+        }
+
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blog> Blog { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=DESKTOP-VC7OLJC;Initial Catalog=Blogging;Integrated Security=True;Pooling=False");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.Property(e => e.DateCreated).HasColumnType("date");
+
+                entity.Property(e => e.Url).IsRequired();
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Post)
+                    .HasForeignKey(d => d.BlogId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
